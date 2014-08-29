9b8bedc94badaa021731aee12ec7977cde8f7bbb
Abhiram <abhi.darkness@gmail.com>
2014-08-29 20:07:12 +0530

    First commit

---
+++ downloadimages.py
@@ -0,0 +1,7 @@
+
+import urllib
+count=1
+while count < 31:
+    urllib.urlretrieve("https://newevolutiondesigns.com/images/freebies/black-wallpaper-"+str(count)+".jpg", str(count)+".jpg")
+    urllib.urlretrieve("https://newevolutiondesigns.com/images/freebies/conceptual-wallpaper-"+str(count)+".jpg",str(count)+"s.jpg")
+    count=count+1
