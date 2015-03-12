1123ff66c069a0e1add13468a7019ac31228c7d7
abhiram <abhiram@bluejeans.com>
2015-03-12 14:48:29 +0530

    Standard Fizzbuzz code

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+n=int(raw_input())
+for i in range(1,n+1):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)    
