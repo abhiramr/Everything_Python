76bc1af25acdc2843ab348994fc1845bf4ef0674
Abhiram <abhi.darkness@gmail.com>
2015-04-12 20:18:09 +0530

    Get the entire HP Characters list

---
+++ HP_GetCharacterList.py
@@ -0,0 +1,20 @@
+import re
+from BeautifulSoup import BeautifulSoup
+import random
+import urllib2
+
+html_page3 = urllib2.urlopen("http://www.happychild.org.uk/ifs/wordsearch/harrypottercharactersindex.htm")
+souphp=BeautifulSoup(html_page3)
+lihp=souphp.findAll("font")
+lahp=[]
+for i in lihp:
+    if i.getText()==(i.getText()).upper() and len(i.getText())>1 and ("CHARACTER" not in i.getText()) and ("CHARACTERS" not in i.getText()) and ("(" not in i.getText()):
+        lahp.append(i.getText())
+count=int(random.random()*1000) 
+fname="hpchars"+str(count)+".txt"
+f=open(fname,"w")  
+lahpf=lahp[22:-8]
+for i in lahpf:
+    f.write(i)
+    f.write("\n")
+f.close()     
