58be7b7fc1d4f7b6ea943adb6872c618f7a869f3
Abhiram R <abhi.darkness@gmail.com>
2018-05-01 00:47:10 +0530

    Added experimental code for Airflow

---
+++ Libraries_Tests/airflow/dags/airflow_1.py
@@ -0,0 +1,59 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['abhi.darkness@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('hello_world3', default_args=default_args, schedule_interval=timedelta(1))
+
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    retries=3,
+    dag=dag)
+
+templated_command = """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+"""
+
+t3 = BashOperator(
+    task_id='templated',
+    bash_command=templated_command,
+    params={'my_param': 'Parameter I passed in'},
+    dag=dag)
+
+t2.set_upstream(t1)
+
+# This means that t2 will depend on t1
+# running successfully to run
+# It is equivalent to
+# t1.set_downstream(t2)
+
+t3.set_upstream(t1)
+
+# all of this is equivalent to
+# dag.set_dependency('print_date', 'sleep')
+# dag.set_dependency('print_date', 'templated')
