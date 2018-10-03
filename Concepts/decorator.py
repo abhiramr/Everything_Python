618652c29ff56ec4f3eb611c81c4e6e38dce4112
Abhiram R <abhi.darkness@gmail.com>
2018-10-03 16:37:40 +0530

    Returning parameters from decorator

--- Concepts/decorator.py
+++ Concepts/decorator.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 def decorator_func(func):
     def wrapper(*args, **kwargs):
-        if 7 <= datetime.now().hour < 16:
-            func(*args, **kwargs)
+        if 7 <= datetime.now().hour < 18:
+            #func(*args, **kwargs)
+            return func(*args,**kwargs)
         else:
             pass  # Hush, the neighbors are asleep
     return wrapper
--- Concepts/decorator_call_2.py
+++ Concepts/decorator_call_2.py
@@ -1,9 +1,23 @@
 ##Illustrating the decorator call with arguments concept
 # Example 1
 from decorator import decorator_func
+import pytest
 
 @decorator_func
 def dec_call3(name):
     print(f"Hi this is the diff file call by {name}")
 
+@decorator_func
+def dec_call4(name):
+    print(f"Hi this is the diff file second call by {name}. Returning YAY")
+    return "YAY"
+
+
 dec_call3("Abhiram")
+ret_val = dec_call4("Kratos")
+print("Ret val = "+str(ret_val))
+
+try:
+    assert ret_val == None
+except:
+    assert ret_val == "YAY"
